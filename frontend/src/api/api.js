a5997e1194fc2f19b83e35914ac451feadd009d9
ManuchehrTG <toshmatovmanuchehr@gmail.com>
2026-01-29 22:48:51 +0500

    update v 1.10

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-console.log('VITE_API_URL:', import.meta.env.VITE_API_URL);
-console.log('All env:', import.meta.env);
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
